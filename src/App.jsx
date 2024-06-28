25bfb1c1018bebc6dd0b6c44f33215c4b63857f1
Ibrahim Şancı <ibrahimsanci613@gmail.com>
2024-06-28 22:41:59 +0300

    28.06.2024

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  let a = 15;
+  const firstName = "Enes";
+  let vize1 = 60;
+  let vize2 = 80;
+  let sonuc = true;
+  let isimler = ["Enes", "Ayşenur", "Kübra", "Adem"];
+
+  return (
+    <div>
+      <p>a değişkeninin değeri : {a} </p>
+      <p>Müşterinin adı : {firstName} </p>
+      <p>Ortalama : {(vize1 + vize2) / 2} </p>
+      {sonuc ? <p>Ehliyeti alabilirsin</p> : <p>Ehliyeti alamazsın</p>}
+      {(vize1 + vize2) / 2 >= 50 ? (
+        <p>Dersten Geçtin </p>
+      ) : (
+        <p>Dersten Kaldın</p>
+      )}
+      {
+        isimler.map((isim, index) =>(
+          <div key={index}>{isim} </div>
+        ))
+      }
+    </div>
+  );
+}
+
+export default App;
